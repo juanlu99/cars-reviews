e15f5fb8df32903b5872c7990ed6bb6b03fc3c47
juanlu99 <j.l.mendez.alvarez@gmail.com>
2021-11-25 18:45:03 +0100

    Comments added

--- app/controllers/cars/get-car-by-id-controller.js
+++ app/controllers/cars/get-car-by-id-controller.js
@@ -4,6 +4,7 @@ const { findCarByID } = require('../../repositories/cars-repository');
 
 function getCarByID(req, res) {
   const { id } = req.params;
+  // si existe el id, si es un numero
   const car = findCarByID(id);
   res.status(200);
   res.send({ data: car });

72ed25577da01d1189678a21af85473664b99478
juanlu99 <j.l.mendez.alvarez@gmail.com>
2021-11-30 21:43:00 +0100

    Added createReviewByCarID & getReviewByCarID

--- app/routes/cars-routes.js
+++ app/routes/cars-routes.js
@@ -6,7 +6,7 @@ const getCars = require('../controllers/cars/get-cars-controller');
 const getCarByID = require('../controllers/cars/get-car-by-id-controller');
 const createReviewByCarID = require('../controllers/cars/create-review-by-car-id-controller');
 const validateAuth = require('../middlewares/validate-auth');
-const getReviewByCarID = require('../controllers/cars/get-review-by-car-id-controller');
+const getReviewByCarID = require('../controllers/cars/get-reviews-by-car-id-controller');
 
 //publicos
 router.route('/').get(getCars);
